3cce0a4b97098f72635146a7f64560151630b874
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2024-03-13 19:05:10 +0200

    fix pagination current and disable

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -15,14 +15,14 @@ export const Pagination = ({ totalPages, serviceClass }) => {
     const [searchParams, setSearchParams] = useSearchParams();
     const [page, setPage] = useState(1);
     const query = searchParams.get('query');
+    const currentPage = Number(searchParams.get('page'));
 
     useEffect(() => {
-        const p = Number(searchParams.get('page'));
-        let res = p;
-        if (p === 1) res = p + 1;
-        if (p === totalPages) res = p - 1;
+        let res = currentPage;
+        if (currentPage === 1) res = currentPage + 1;
+        if (currentPage === totalPages) res = currentPage - 1;
         setPage(res);
-    }, [searchParams, totalPages]);
+    }, [currentPage, totalPages]);
 
     const hendleClick = e => {
         const toPage = e.target.getAttribute('datatype');
@@ -32,38 +32,66 @@ export const Pagination = ({ totalPages, serviceClass }) => {
     return (
         <PaginationBox className={serviceClass}>
             {totalPages > 3 && (
-                <DotStart datatype={1} onClick={hendleClick}>
-                    <Svg use={`${sprite}#arrow-start`} />
+                <DotStart
+                    disabled={currentPage === 1}
+                    datatype={1}
+                    onClick={hendleClick}
+                >
+                    <Svg w={20} h={20} use={`${sprite}#arrow-start`} />
                 </DotStart>
             )}
             {totalPages > 3 && (
-                <DotPrev datatype={Number(page) - 1} onClick={hendleClick}>
-                    <Svg use={`${sprite}#arrow-prev`} />
+                <DotPrev
+                    disabled={currentPage === 1}
+                    datatype={Number(page) - 1}
+                    onClick={hendleClick}
+                >
+                    <Svg w={20} h={20} use={`${sprite}#arrow-prev`} />
                 </DotPrev>
             )}
             {totalPages > 1 && (
-                <Dot onClick={hendleClick} datatype={Number(page) - 1}>
+                <Dot
+                    aria-current={currentPage === Number(page) - 1}
+                    onClick={hendleClick}
+                    datatype={Number(page) - 1}
+                >
                     {Number(page) - 1}
                 </Dot>
             )}
             {totalPages > 1 && (
-                <Dot onClick={hendleClick} datatype={Number(page)}>
+                <Dot
+                    aria-current={currentPage === Number(page)}
+                    onClick={hendleClick}
+                    datatype={Number(page)}
+                >
                     {Number(page)}
                 </Dot>
             )}
             {totalPages > 2 && (
-                <Dot onClick={hendleClick} datatype={Number(page) + 1}>
+                <Dot
+                    aria-current={currentPage === Number(page) + 1}
+                    onClick={hendleClick}
+                    datatype={Number(page) + 1}
+                >
                     {Number(page) + 1}
                 </Dot>
             )}
             {totalPages > 3 && (
-                <DotNext datatype={Number(page) + 1} onClick={hendleClick}>
-                    <Svg use={`${sprite}#arrow-next`} />
+                <DotNext
+                    disabled={currentPage === totalPages}
+                    datatype={Number(page) + 1}
+                    onClick={hendleClick}
+                >
+                    <Svg w={20} h={20} use={`${sprite}#arrow-next`} />
                 </DotNext>
             )}
             {totalPages > 3 && (
-                <DotEnd onClick={hendleClick} datatype={totalPages}>
-                    <Svg use={`${sprite}#arrow-end`} />
+                <DotEnd
+                    disabled={currentPage === totalPages}
+                    onClick={hendleClick}
+                    datatype={totalPages}
+                >
+                    <Svg w={20} h={20} use={`${sprite}#arrow-end`} />
                 </DotEnd>
             )}
         </PaginationBox>
--- src/components/Pagination/Pagination.styled.js
+++ src/components/Pagination/Pagination.styled.js
@@ -16,19 +16,34 @@ export const DotButton = styled.button`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 40px;
-    height: 40px;
-    transition: background-color 250ms;
+    width: 32px;
+    height: 32px;
+    border-radius: 6px;
+    color: ${({ theme }) => theme.colors.light};
+    transition: background-color ${({ theme }) => theme.baseTransition},
+        color ${({ theme }) => theme.baseTransition};
     background-color: ${({ theme }) => theme.colors.hover};
+    & svg {
+        fill: currentColor;
+    }
     &:hover,
-    &:focus {
+    &[aria-current='true'] {
+        color: ${({ theme }) => theme.colors.white};
         background-color: ${({ theme }) => theme.colors.accent};
     }
+    @media ${({ theme }) => theme.breakpoints.xs} {
+        width: 40px;
+        height: 40px;
+    }
+    &:disabled {
+        cursor: not-allowed;
+        color: #636363;
+        background-color: #4a4a4a;
+    }
 `;
+
 export const DotStart = styled(DotButton)``;
 export const DotEnd = styled(DotButton)``;
 export const DotPrev = styled(DotButton)``;
 export const DotNext = styled(DotButton)``;
-export const Dot = styled(DotButton)`
-    color: ${({ theme }) => theme.colors.white};
-`;
+export const Dot = styled(DotButton)``;

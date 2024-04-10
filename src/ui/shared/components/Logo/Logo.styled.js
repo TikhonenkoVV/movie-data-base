7b22c7b342af4e44a8a86cba18014387fe846890
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2024-04-10 11:52:53 +0300

    change logp

--- src/ui/shared/components/Logo/Logo.styled.js
+++ src/ui/shared/components/Logo/Logo.styled.js
@@ -5,15 +5,15 @@ import logoM from '../../../assets/images/logo-m.svg';
 export const Logotype = styled(Link)`
     flex-shrink: 0;
     display: flex;
-    width: 80px;
-    aspect-ratio: 2 / 1;
+    width: 90px;
+    aspect-ratio: 3.125 / 1;
     background-image: url(${logoM});
     background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
     &.footer-logo {
         width: 90px;
-        aspect-ratio: 2.3346 / 1;
+        aspect-ratio: 3.125 / 1;
         margin-left: auto;
         margin-right: auto;
         margin-bottom: 24px;
